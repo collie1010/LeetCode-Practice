4729b2aad804aea24b531fd610452f288b1ffc54
HW-T06 <asd640426@gmail.com>
2025-04-24 14:00:04 +0800

    Append InvertTree

--- demo/src/main/java/BFS/InvertTree.java
+++ demo/src/main/java/BFS/InvertTree.java
@@ -1,5 +1,32 @@
 package BFS;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class InvertTree {
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null)
+			return null;
+
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(root);
+
+		while (!queue.isEmpty()) {
+			TreeNode current = queue.poll();
+
+			// 交換左右子樹
+			TreeNode temp = current.left;
+			current.left = current.right;
+			current.right = temp;
+
+			// 將左右子節點加入隊列
+			if (current.left != null)
+				queue.add(current.left);
+			if (current.right != null)
+				queue.add(current.right);
+		}
+
+		return root;
+	}
 
 }
