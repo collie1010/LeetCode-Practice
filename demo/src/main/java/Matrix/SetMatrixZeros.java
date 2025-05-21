89b254c5c905771d6782c59d29d686ffc1a973f5
HW-T06 <asd640426@gmail.com>
2025-05-21 08:54:21 +0800

    Update SetMatrixZeros.java

--- demo/src/main/java/Matrix/SetMatrixZeros.java
+++ demo/src/main/java/Matrix/SetMatrixZeros.java
@@ -55,4 +55,26 @@ public class SetMatrixZeros {
             }
         }
     }
+    
+    public void setZeroes2(int[][] matrix) {
+        boolean[] rows = new boolean[matrix.length];
+        boolean[] cols = new boolean[matrix[0].length];
+        // this two boolean array is used for wheather the row or column exist a 0 value
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (rows[i] || cols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
 }
