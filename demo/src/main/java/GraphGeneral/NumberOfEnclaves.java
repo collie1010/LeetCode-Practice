f72b3df236482e1e2087515e99394357e6caeec6
HW-T06 <asd640426@gmail.com>
2025-08-20 12:17:11 +0800

    Add NumberOfEnclaves solution
    Implements the NumberOfEnclaves class with a method to count the number of enclaves in a grid using DFS. This provides a solution for identifying land cells not connected to the boundary.

---
+++ demo/src/main/java/GraphGeneral/NumberOfEnclaves.java
@@ -0,0 +1,30 @@
+package GraphGeneral;
+
+public class NumberOfEnclaves {
+    public int numEnclaves(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if ((i == 0 || j == 0 || i == grid.length - 1 || j == grid[0].length - 1) && grid[i][j] == 1) 
+                    dfs(grid, i, j);
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                result += grid[i][j];
+            }
+        }
+        return result;
+    }
+
+    private void dfs(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) return;
+        grid[i][j] = 0;
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+        return;
+    }
+}
