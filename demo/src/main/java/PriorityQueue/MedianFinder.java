a7a4534b083c4a515258b5abe017222f5b699f6e
wallace.hsiao <asd640426@gmail.com>
2025-10-16 15:19:36 +0800

    Add MedianFinder class using two heaps
    Introduces the MedianFinder class that maintains a data stream and efficiently computes the median using a max-heap and a min-heap.

---
+++ demo/src/main/java/PriorityQueue/MedianFinder.java
@@ -0,0 +1,25 @@
+package PriorityQueue;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class MedianFinder {
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+    public MedianFinder() {
+    }
+
+    public void addNum(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (maxHeap.size() > minHeap.size()) return maxHeap.peek();
+        return (maxHeap.peek() + minHeap.peek()) / 2.0;
+    }
+}
